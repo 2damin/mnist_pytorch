b6bfff49e471941430e31719eedb529bc52841a5
2damin <leesak2003@naver.com>
2022-03-15 00:20:55 +0900

    add vgg

--- model/models.py
+++ model/models.py
@@ -1,8 +1,11 @@
 
+from model.vgg16 import VGG16
 from .lenet5 import lenet5
 
 def get_model(model_name):
     if(model_name == "lenet5"):
         return lenet5
+    elif(model_name == "vgg16"):
+        return VGG16
     else:
         print("unknown model")
